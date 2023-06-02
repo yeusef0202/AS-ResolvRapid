4955496876ab5b1f9934db73b17c1b5c78ded42f
rafaelpereira014 <rafaelbotelhopereira@ua.pt>
2023-06-02 20:58:30 +0100

    add loggin auth and store the values

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,30 +1,91 @@
+import { useState } from 'react';
+import './pages.css';
 import { useNavigate } from 'react-router-dom';
 
 export function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
   const navigate = useNavigate();
 
-  function gotoHome() {
-    navigate('/home')
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+   
   }
-  function gotoBegin() {
-    navigate('/')
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+
+  }
+
+  function handleConfirmPasswordChange(event) {
+    setConfirmPassword(event.target.value);
+    
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if(!(email.includes('@') && email.includes('.'))){
+      console.log('Invalid email.');
+      alert('Invalid email.');
+      return;
+    }
+
+  
+    if (password !== confirmPassword) {
+      console.log('Passwords do not match.');
+      alert('Passwords do not match.');
+      return;
+    }
+  
+    alert('Account created successfully!');
+    // Store the registered email in localStorage
+    localStorage.setItem('registeredEmail', email);
+    localStorage.setItem('registeredPassword', password);
+
+
+    // Reset the form
+    setEmail('');
+    setPassword('');
   }
   
-    return ( 
-        <div className="bg">
-          <div className="home-holder">
-            <div className="logo">
-              <p className='logo-align'><span style={{fontSize:'35px'}}>RESOLV</span><br></br><span style={{fontFamily: 'postnobills'}}>RAPID</span></p>
-            </div>
-            <form action="" className="form-control" style={{flex:'1.45'}}>
-              <input type="text" placeholder='Email' />
-              <input type="password" placeholder='Password'/>
-              <input type="password" placeholder='Confirm Password'/>      
-              <button onClick={gotoHome}>Sign Up</button>
-              <p className='login-text'>Already have an account yet?</p>
-              <a onClick={gotoBegin}>Sign in to your account</a>
-            </form>
-          </div>
+
+  return (
+    <div className="bg">
+      <div className="home-holder">
+        <div className="logo">
+          <p className='logo-align'>
+            <span style={{ fontSize: '35px' }}>RESOLV</span>
+            <br></br>
+            <span style={{ fontFamily: 'postnobills' }}>RAPID</span>
+          </p>
         </div>
-     );
+        <form onSubmit={handleSubmit} className="form-control" style={{ flex: '1.45' }}>
+          <input
+            type="text"
+            placeholder='Email'
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <input
+            type="password"
+            placeholder='Password'
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <input
+            type="password"
+            placeholder='Confirm Password'
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+          />
+          <button type="submit">Sign Up</button>
+          <p className='login-text'>Already have an account yet?</p>
+          <a href='/'>Sign in to your account</a>
+        </form>
+      </div>
+    </div>
+  );
 }
