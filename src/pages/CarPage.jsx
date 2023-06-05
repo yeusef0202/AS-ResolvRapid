39e49440c626004077e4f5a2ca8765f3854efe1b
rafaelpereira014 <rafaelbotelhopereira@ua.pt>
2023-06-05 03:04:27 +0100

    remove carros

--- src/pages/CarPage.jsx
+++ src/pages/CarPage.jsx
@@ -6,12 +6,11 @@ import { useNavigate } from 'react-router-dom';
 
 import './carros.css';
 
-
 export function CarPage() {
   const navigate = useNavigate();
 
   function gotoQR() {
-    navigate('/QR')
+    navigate('/QR');
   }
 
   const { id } = useParams();
@@ -21,26 +20,56 @@ export function CarPage() {
     // Retrieve carros from cookies
     const storedCarros = Cookies.get('carros');
     const parsedCarros = storedCarros ? JSON.parse(storedCarros) : [];
-    
+
     // Retrieve the specific car based on the id
     const selectedCarro = parsedCarros[id] || '';
     setCarro(selectedCarro);
   }, [id]);
 
+  function eliminarCarro() {
+    // Retrieve carros from cookies
+    const storedCarros = Cookies.get('carros');
+    const parsedCarros = storedCarros ? JSON.parse(storedCarros) : [];
+
+    // Remove the car at the specified id
+    parsedCarros.splice(id, 1);
+
+    // Update the carros array in cookies
+    Cookies.set('carros', JSON.stringify(parsedCarros));
+
+    // Navigate back to the carros page
+    navigate('/carros');
+  }
+
   return (
     <div className="bg">
       <div className="holder-carros">
         <div className="carrinho">
-        <div className="logo" style={{ fontFamily: 'postnobills', fontSize: '55px', margin: '20px 0px',fontWeight:'400' }}><b>Resolv</b>Rapid</div>
+          <div className="logo" style={{ fontFamily: 'postnobills', fontSize: '55px', margin: '20px 0px', fontWeight: '400' }}>
+            <b>Resolv</b>Rapid
+          </div>
           <div className="infos">
-            <div className="info-item"><span style={{fontWeight:'600'}}>Matrícula:</span> {carro.matricula}</div>
-            <div className="info-item"><span style={{fontWeight:'600'}}>Marca:</span> {carro.marca}</div>
-            <div className="info-item"><span style={{fontWeight:'600'}}>Modelo: </span>{carro.modelo}</div>
-            <div className="info-item"><span style={{fontWeight:'600'}}>Pais:</span> {carro.pais}</div>
-            <div className="info-item"><span style={{fontWeight:'600'}}>Apólice:</span> {carro.apolice}</div>
+            <div className="info-item">
+              <span style={{ fontWeight: '600' }}>Matrícula:</span> {carro.matricula}
+            </div>
+            <div className="info-item">
+              <span style={{ fontWeight: '600' }}>Marca:</span> {carro.marca}
+            </div>
+            <div className="info-item">
+              <span style={{ fontWeight: '600' }}>Modelo:</span> {carro.modelo}
+            </div>
+            <div className="info-item">
+              <span style={{ fontWeight: '600' }}>Pais:</span> {carro.pais}
+            </div>
+            <div className="info-item">
+              <span style={{ fontWeight: '600' }}>Apólice:</span> {carro.apolice}
+            </div>
           </div>
           <div className='botao-para-qr'>
-            <button onClick={gotoQR}>Transferir informações</button>
+          <button onClick={gotoQR}>Transferir informações</button>
+          <div className='eliminar-para-carro'>
+            <button onClick={eliminarCarro}> Eliminar Carro </button>
+          </div>
           </div>
         </div>
         <Footer />
